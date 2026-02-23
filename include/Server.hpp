1844739becc2b16ff525ab1fe82ddbab872e8bf0
patsyMn <patsy.meimoun@laposte.net>
2026-02-23 13:04:33 +0100

    server done

--- include/Server.hpp
+++ include/Server.hpp
@@ -6,7 +6,7 @@
 /*   By: pmeimoun <pmeimoun@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/20 11:44:07 by zamohame          #+#    #+#             */
-/*   Updated: 2026/02/20 14:14:34 by pmeimoun         ###   ########.fr       */
+/*   Updated: 2026/02/23 12:56:31 by pmeimoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,26 +18,29 @@
 #include <sys/types.h> // le type ici sock_stream pour recevoir les messages dans lordre et ne pas perdre une partie du message
 // IPPROTO_TCP  // pour TCP
 #include <iostream>
-#include <stdexcept> 
+#include <stdexcept>
 #include <fcntl.h>
 #include <cerrno>
+#include <vector>
+#include <poll.h>
+#include <unistd.h>
 
-class Server 
+class Server
 {
-    private:
-        std::string password;
+	private:
+		std::string password;
 		int server_fd;
+		std::vector<pollfd> connections;
 
-    public:
-        Server();
-        Server(Server const &src);
-        Server &operator=(Server const &other);
-        ~Server();
+	public:
+		Server();
+		Server(Server const &src);
+		Server &operator=(Server const &other);
+		~Server();
 
 		void createSocket();
-		void Server::serverLoop();
-        
-    
+		void serverLoop();
+		void acceptNewClient();
+		void readClientMessage(int client_fd);
 };
 
-#endif
--- src/Server.cpp
+++ src/Server.cpp
@@ -21,33 +21,72 @@ void Server::createSocket()
 
 	if(listen(server_fd, SOMAXCONN) < 0)
 		throw std::runtime_error("Error: Could not listen");
-	
+
 	if(fcntl(server_fd, F_SETFL, O_NONBLOCK) < 0)
 		throw std::runtime_error("Error: Failed to set socket options");
 }
 
 void Server::serverLoop()
 {
-	while(true)
+	while (true)
 	{
-		struct sockaddr_in client_address;
-		socklen_t client_len = sizeof(client_address);
-		int client_fd = accept(server_fd, (struct sockaddr*)&client_address, &client_len);
-		if (client_fd < 0)	
+		if (poll(&connections[0], connections.size(), -1) == -1)
+			throw std::runtime_error("Error: poll() failed");
+		for (size_t i = 0; i < connections.size(); i++)
 		{
-			if(errno == EAGAIN) 
-			continue;
+			if (connections[i].revents & POLLIN)
+			{
+				if (connections[i].fd == server_fd)
+					acceptNewClient();
+				else
+					readClientMessage(connections[i].fd);
+			}
 		}
-		else
-		{
-			std::cerr << "Error: Failed to accept client, server stopped." << std::endl;
-			break;
-		}
-		
-			
-		
-
 	}
+}
 
+void Server::acceptNewClient()
+{
+	while (true)
+	{
+		sockaddr_in client_address;
+		socklen_t client_len = sizeof(client_address);
+		int client_fd = accept(server_fd, (sockaddr*)&client_address, &client_len);
+		if (client_fd < 0)
+		{
+			if (errno == EAGAIN || errno == EWOULDBLOCK)
+				break;
+			else
+				throw std::runtime_error("accept() failed");
+		}
+		fcntl(client_fd, F_SETFL, O_NONBLOCK); pollfd clientPoll;
+		clientPoll.fd = client_fd;
+		clientPoll.events = POLLIN;
+		clientPoll.revents = 0;
+		connections.push_back(clientPoll);
+		std::cout << "New client connected: " << client_fd << std::endl;
+	}
+}
 
+void Server::readClientMessage(int client_fd)
+{
+	char buffer[1024] = {};
+	int bytesRead = recv(client_fd, buffer, sizeof(buffer), 0);
+	if (bytesRead <= 0)
+	{
+		close(client_fd);
+		for (size_t i = 0; i < connections.size(); i++)
+		{
+			if (connections[i].fd == client_fd)
+			{
+				connections.erase(connections.begin() + i);
+				break;
+			}
+		}
+	}
+	else
+	{
+		std::string reply = "Message received!\r\n";
+		send(client_fd, reply.c_str(), reply.size(), 0);
+	}
 }
